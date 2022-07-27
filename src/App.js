1db244f9005d2f91cd921913bec74fe72ee1f412
Marcin Bieniek <marcingim@gmail.com>
2022-07-27 12:23:14 +0200

    Add project basic files and logic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from "./components/pages/Home";
+import SinglePost from "./components/pages/SinglePost";
+import AddPost from './components/pages/AddPost';
+import EditPost from './components/pages/EditPost';
+import About from './components/pages/About';
+import NotFound from './components/pages/NotFound';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    
+    <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/post/:id" element={<SinglePost />} />
+          <Route path="/post/add" element={<AddPost />} />
+          <Route path="/post/edit/:id" element={<EditPost />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+    </main>
+
+  )
 }
 
 export default App;
+
+
+
---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,7 @@
+const NotFound = () => {
+    return (
+        <h1>Sorry, page don't exist</h1>
+    );
+};
+
+export default NotFound;
